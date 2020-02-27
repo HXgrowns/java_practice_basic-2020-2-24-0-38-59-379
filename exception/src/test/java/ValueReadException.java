eb2447b79921184b4bae0b3689b53a1924ef36c6
huxiao <804156017@qq.com>
2020-02-27 17:59:44 +0800

    [huxiao] {modify}: modify ValueReadException

--- exception/src/test/java/ValueReadException.java
+++ exception/src/test/java/ValueReadException.java
@@ -1,36 +1,16 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class ValueReadException extends RuntimeException {
-    private String message;
-    private Throwable cause;
-
     public ValueReadException(String message) {
 
         //TODO: change the code to pass the test
-        this.message = message;
+        super(message);
     }
 
     public ValueReadException(String message, Throwable cause) {
         //TODO: change the code to pass the test
-        this.message = message;
-        this.cause = cause;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public Throwable getCause() {
-        return cause;
-    }
-
-    public void setCause(Throwable cause) {
-        this.cause = cause;
+        super(message, cause);
     }
 }
+
+
